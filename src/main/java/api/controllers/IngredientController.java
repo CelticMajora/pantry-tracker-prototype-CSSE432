a5ac5dfa9668d0e6b52fa31f08467f3ac324d4a6
donohoc1 <donohoc1@rose-hulman.edu>
2019-04-16 22:50:54 -0400

    Added the frame for POST and DELETE

--- src/main/java/api/controllers/IngredientController.java
+++ src/main/java/api/controllers/IngredientController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +15,29 @@ public class IngredientController {
 
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/ingredient")
-	public Ingredient ingredient(@RequestParam(value = "name", defaultValue = "defaultIngredient") String name,
+	@RequestMapping(value = "/ingredient", method = RequestMethod.GET)
+	public Ingredient getIngredient(@RequestParam(value = "name", defaultValue = "defaultIngredient") String name,
 			@RequestParam(value = "expirationYear", defaultValue = "0") String expirationYear,
 			@RequestParam(value = "expirationMonth", defaultValue = "1") String expirationMonth,
 			@RequestParam(value = "expirationDayOfMonth", defaultValue = "1") String expirationDayOfMonth) {
 		return new Ingredient(counter.incrementAndGet(), name, LocalDate.of(Integer.parseInt(expirationYear),
 				Integer.parseInt(expirationMonth), Integer.parseInt(expirationDayOfMonth)));
+	}
+	
+	@RequestMapping(value = "/ingredient", method = RequestMethod.POST)
+	public void postIngredient(@RequestParam(value = "name") String name) {
+		if(name == null) {
+			//TODO send 500
+		}
+		//TODO store new Ingredient and return 200
+	}
+	
+	@RequestMapping(value = "/user", method = RequestMethod.DELETE)
+	public void deleteIngredient(@RequestParam(value = "name") String name) {
+		if(name == null) {
+			//TODO send 500
+		}
+		//TODO delete Ingredient and return 200
 	}
 
 }
--- src/main/java/api/controllers/UserController.java
+++ src/main/java/api/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,26 @@ public class UserController {
 
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/user")
-	public User user(@RequestParam(value = "name", defaultValue = "defaultName") String name) {
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public User getUser(@RequestParam(value = "name", defaultValue = "defaultName") String name) {
+		//TODO change param to id and return out of database
 		return new User(counter.incrementAndGet(), name, new LinkedList<Ingredient>());
+	}
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public void postUser(@RequestParam(value = "name") String name) {
+		if(name == null) {
+			//TODO send 500
+		}
+		//TODO store new User and return 200
+	}
+	
+	@RequestMapping(value = "/user", method = RequestMethod.DELETE)
+	public void deleteUser(@RequestParam(value = "name") String name) {
+		if(name == null) {
+			//TODO send 500
+		}
+		//TODO delete User and return 200
 	}
 
 }
