67aa91889efdf2df54fc749c7a2b33628243cc2f
Keith Hanson <hanson2@rose-hulman.edu>
2019-05-07 17:49:49 -0400

    added basic method stubs and core of the javascript file

--- src/main/java/api/views/app.js
+++ src/main/java/api/views/app.js
@@ -1,3 +1,47 @@
 /**
  * 
  */
+const app = {
+		
+		//defined at bottom of page
+		init(selectors){
+			this.user = -1;
+			
+			this.loginForm = document.querySelector(seletors.loginFormSelector);
+			this.loginForm.addEventListener('submit', this.handleLogin.bind(this));
+			
+			this.ingredients = document.querySelector(selectors.ingredientsSelector);
+			
+			this.friends = document.querySelector(selectors.friendsSelector);
+		},
+		
+		//form.querySelector
+		//event ev 
+		handleLogin(form, ev){
+			//get userID from form
+			//fill out the user object
+		},
+		
+		//User user
+		renderUser(user){
+			
+		},
+		
+		//Ingredient ingredient
+		renderIngredient(ingredient){
+			
+		},
+		
+		//User user
+		renderFriend(user){
+			
+		},
+		
+		
+}
+
+app.init({
+	loginFormSelector: "",
+	ingredientsSelector: "",
+	friendsSelector: "",
+})
