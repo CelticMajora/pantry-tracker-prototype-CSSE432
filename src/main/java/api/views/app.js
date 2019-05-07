6d25e46838b0260083d9177136576bd856904dee
Keith Hanson <hanson2@rose-hulman.edu>
2019-05-07 18:50:17 -0400

    added some correlation between the js and the html

--- src/main/java/api/views/app.js
+++ src/main/java/api/views/app.js
@@ -1,6 +1,9 @@
 /**
  * 
  */
+
+const URL = '127.0.0.1:8080/';
+
 const app = {
 		
 		//defined at bottom of page
@@ -20,21 +23,44 @@ const app = {
 		handleLogin(form, ev){
 			//get userID from form
 			//fill out the user object
+			
+			//ev.preventDefault();
+			const id = form.querySelector('.userID').value;
+			//use fetch to do API calls
+			
 		},
 		
+		//handles filling in the page for that users data
 		//User user
 		renderUser(user){
+			//display user info?
 			
+			//get friends
+			//render all friends
+			
+			//get ingredients
+			//render all ingredients
 		},
 		
-		//Ingredient ingredient
+		//Ingredient ingredient (API object)
 		renderIngredient(ingredient){
+			const item = document.querySelector('#contents li.template').cloneNode(true);
+			item.classList.remove('template');
+			item.querySelector('.name').textContent = ingredient.name;
 			
+			item.id = `ingred-id-${ingredient.id}`;
+			//querySelect buttons and add listeners later (see MichaelBaywatch)
+			this.ingredients.insertBefore(item, this.ingredients.firstChild);
 		},
 		
-		//User user
+		//User user (API object)
 		renderFriend(user){
+			const item = document.querySelector('#friend li.template').cloneNode(true);
+			item.classList.remove('template');
+			item.querySelector('.name').textContent = user.name;
+			item.querySelector('.userID').textContent = user.ID;
 			
+			this.friends.insertBefore(item, this.friends.firstChild);
 		},
 		
 		
@@ -42,6 +68,6 @@ const app = {
 
 app.init({
 	loginFormSelector: "",
-	ingredientsSelector: "",
-	friendsSelector: "",
+	ingredientsSelector: "#contents li",
+	friendsSelector: "#friends li",
 })
