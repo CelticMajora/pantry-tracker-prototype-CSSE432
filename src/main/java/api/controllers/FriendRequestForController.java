783b82915d73c5ba380261abd5999de51844e160
Keith Hanson <hanson2@rose-hulman.edu>
2019-05-15 17:54:40 -0400

    added friends and friend request to the initial render of a user

--- src/main/java/api/controllers/FriendRequestForController.java
+++ src/main/java/api/controllers/FriendRequestForController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import api.repositories.FriendRequestForRepository;
 import api.repositories.FriendsWithRepository;
 import api.repositories.UserRepository;
 
+@CrossOrigin(origins = "http://127.0.0.1:8081")
 @RestController
 public class FriendRequestForController {
 
--- src/main/java/api/views/app.js
+++ src/main/java/api/views/app.js
@@ -44,11 +44,50 @@ const app = {
 			
 		},
 		
+		//event ev
+		sendFriendRequest(ev){
+		},
+		
+		//event ev
+		addUser(ev){
+//			ev.preventDefault();
+//			fetch(URL+'user/?name='+ev.target.querySelector('.name').value,
+//					{headers:new Headers({
+//						'Access-Control-Allow-Origin': '*',
+//					}),
+//					method:'post',
+//					}).then((obj)=>obj.json())
+//					.then(function(obj) = {
+//						console.log(obj);
+//					})
+//					.catch(function(error){
+//						console.log(error);
+//					});
+			
+			const user = {
+					id:'get id from post response',
+					name:ev.target.querySelector('.name').value,
+			}
+			this.renderUser(user);
+		},
+		
+		//event ev
+		acceptFriendRequest(ev){
+			ev.preventDefault();
+			ev.target.parentElement;
+		},
+		
+		//event ev
+		rejectFriendRequest(ev){
+			ev.preventDefault();
+			ev.target.parentElement;
+		},
+		
 		//handles filling in the page for that users data
 		//User user
 		renderUser(user){
 			//display user info?
-			
+			this.user = user;
 			//get friends
 			console.log(user);
 			console.log('id:'+user.id);
@@ -76,6 +115,35 @@ const app = {
 					.catch(function(error){
 						console.log(error);
 					});
+			
+			//get friendRequests recieved
+			//render all friendRequests
+			fetch(URL+'friend_requests/received?userId='+user.id,
+					{headers:new Headers({
+						'Access-Control-Allow-Origin': '*',
+					}),
+					}).then((obj)=>obj.json())
+					.then(this.renderAllFriend_Requests.bind(this))
+					.catch(function(error){
+						console.log(error);
+					});
+		},
+		
+		renderAllFriend_Requests(requests){
+			for(const rqst of requests){
+				this.renderFriendRequest(rqst);
+			}
+		},
+		
+		renderFriendRequest(request){
+			const item = document.querySelector('#contents li.template').cloneNode(true);
+			item.classList.remove('template');
+			
+			item.querySelector('.userId').textContent = request.userId;
+			item.querySelector('.id').textContent = request.id;
+			item.querySelector('.accept').addEvenListener('click', this.acceptFriendRequest.bind(this));
+			item.querySelector('.reject').addEvenListener('click', this.rejectFriendRequest.bind(this));
+			
 		},
 		
 		//JSON list of ingredient objects
@@ -87,12 +155,11 @@ const app = {
 		
 		//Ingredient ingredient (API object)
 		renderIngredient(ingredient){
-			const item = document.querySelector('#contents li.template').cloneNode(true);
+			const item = document.querySelector('#ingredientList li.template').cloneNode(true);
 			item.classList.remove('template');
 			item.querySelector('.name').textContent = ingredient.name;
+			item.querySelector('.id') = ingredient.id;
 			
-			item.id = `ingred-id-${ingredient.id}`;
-			//querySelect buttons and add listeners later (see MichaelBaywatch)
 			this.ingredients.insertBefore(item, this.ingredients.firstChild);
 		},
 		
@@ -105,10 +172,12 @@ const app = {
 		
 		//User user (API object)
 		renderFriend(user){
-			const item = document.querySelector('#friend li.template').cloneNode(true);
+			const item = document.querySelector('#friendsList li.template').cloneNode(true);
 			item.classList.remove('template');
 			item.querySelector('.name').textContent = user.name;
 			item.querySelector('.userID').textContent = user.ID;
+			//TODO
+			//item.querySelector('.delete').addActionListener(this.deleteFriend.bind(this));
 			
 			this.friends.insertBefore(item, this.friends.firstChild);
 		},
@@ -118,6 +187,6 @@ const app = {
 
 app.init({
 	loginFormSelector: "form.login",
-	ingredientsSelector: "#contents li",
-	friendsSelector: "#friends li",
+	ingredientsSelector: "#ingredientList li",
+	friendsSelector: "#friendsList li",
 })
